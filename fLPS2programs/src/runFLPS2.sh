7280294feade3a26fdbc4c86b5b7bbc6215bc239
jhovan <julienh15@icloud.com>
2023-03-22 13:55:05 -0400

    Initial commit

---
+++ fLPS2programs/src/runFLPS2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set the input folder and command
+input_folder="/home/julienh/Desktop/COMP401/Proteomes/Drosophila"
+output_folder="/home/julienh/Desktop/COMP401/Proteomes/Annotated_3"
+command="./fLPS2 -t1e-5 -m5 -m25"
+command2="./fLPS2 -d -o long -c equal -rQ -t 0.000001"
+command3="./fLPS2 -d -o long -c equal -rQPH -t 0.000001"
+
+# Loop through all .txt files in the input folder
+for file in "$input_folder"/*
+do
+  # set output file name
+  echo "$file"
+  output_file="$output_folder/$(basename "$file" .fasta).out"
+  # Run the command on each file
+  $command3 "$file" > "$output_file"
+  sleep 25
+done
+
