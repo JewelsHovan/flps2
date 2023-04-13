c76fe030a99f7b35e690cd6049459063c9b5b5b2
JewelsHovan <60527617+JewelsHovan@users.noreply.github.com>
2023-04-13 12:11:18 -0400

    runFLPS

--- fLPS2programs/src/runFLPS2.sh
+++ fLPS2programs/src/runFLPS2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
 # Set the input folder and command
 input_folder="/home/julienh/Desktop/COMP401/Proteomes/Drosophila"
 output_folder="/home/julienh/Desktop/COMP401/Proteomes/Annotated_3"
 command="./fLPS2 -t1e-5 -m5 -m25"
 command2="./fLPS2 -d -o long -c equal -rQ -t 0.000001"
 command3="./fLPS2 -d -o long -c equal -rQPH -t 0.000001"
+command4="./fLPS2 -m5 -M200 -d -o masked -c drosophila.sequences.fasta.COMPOSITION -rQPH -t1e-15"
+command5="./fLPS2 -m5 -M200 -d -o long -c drosophila.sequences.fasta.COMPOSITION -rQPH -t1e-15"
 
 # Loop through all .txt files in the input folder
 for file in "$input_folder"/*
